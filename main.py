7b5b7076253ce2e11785e856858be9083f0b0df3
Arti Zirk <arti.zirk@gmail.com>
2013-11-06 00:02:31 +0200

    added licence and main program

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+###########################################################################
+#                          Kuule osta elevant ära                         #
+#                                                                         #
+#                                                                         #
+#Author:  Arti Zirk <arti.zirk@gmail.com                                  #
+#License: Do What the Fuck You Want to Public License                     #
+#                                                                         #
+###########################################################################
+
+print("Kuule osta elevant ära!")
+while True:
+    vastus = input("vasta: ")
+    print("Seda räägivad kõik, et " + vastus + " aga osta elevant ära")
