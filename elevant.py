fcc26ffe689070392c9edea81aad5bef43fa86a4
Arti Zirk <arti.zirk@gmail.com>
2014-01-24 19:07:58 +0200

    lisab -h ja --abi võtmed ning mõne muu  sõnaga saab elevandi väljavahetada mõne muu asja vastu

---
+++ elevant.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+########################################################################
+#                     Kuule osta elevant ära                           #
+#                                                                      #
+#                                                                      #
+#Author:  Arti Zirk <arti.zirk@gmail.com>                              #
+#License: Do What the Fuck You Want to Public License                  #
+#                                                                      #
+########################################################################
+
+import sys
+
+asi = " ".join(sys.argv[1:])
+if not  asi.strip():  # argumente pole, kasuta vaikimisi asja
+    asi = "elevant"
+    
+
+abi="""Kuule osta elevant ära
+Kasutus: main.py [asi | -h | --abi]"""
+if asi in ("-h", "--abi",):
+    print(abi)
+    exit(0)
+
+print("> Kuule osta " + asi + " ära!")
+while True:
+    try:
+        vastus = input("< ").lower()
+        print("> Seda räägivad kõik, et " + vastus + " aga osta "+ asi +" ära!")
+    except (EOFError, KeyboardInterrupt) as e:
+        print("\033[4D> Mis sul viga on, et " + asi + " ei taha ??")
+        exit()
+    except:
+        print("Error, mingine kala ujus sisse")
+        exit()
