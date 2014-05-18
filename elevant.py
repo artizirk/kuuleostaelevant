1588e7d727c36d5b70873dfc51041ea99cdda4c0
Arti Zirk <arti.zirk@gmail.com>
2014-05-18 13:50:39 +0300

    pep8 värk

--- elevant.py
+++ elevant.py
@@ -3,19 +3,19 @@
 #                     Kuule osta elevant ära                           #
 #                                                                      #
 #                                                                      #
-#Author:  Arti Zirk <arti.zirk@gmail.com>                              #
-#License: Do What the Fuck You Want to Public License                  #
+# Author:  Arti Zirk <arti.zirk@gmail.com>                              #
+# License: Do What the Fuck You Want to Public License                  #
 #                                                                      #
 ########################################################################
 
 import sys
 
 asi = " ".join(sys.argv[1:])
-if not  asi.strip():  # argumente pole, kasuta vaikimisi asja
+if not asi.strip():  # argumente pole, kasuta vaikimisi asja
     asi = "elevant"
-    
 
-abi="""Kuule osta elevant ära
+
+abi = """Kuule osta elevant ära
 Kasutus: main.py [asi | -h | --abi]"""
 if asi in ("-h", "--abi",):
     print(abi)
@@ -25,7 +25,8 @@ print("> Kuule osta " + asi + " ära!")
 while True:
     try:
         vastus = input("< ").lower()
-        print("> Seda räägivad kõik, et " + vastus + " aga osta "+ asi +" ära!")
+        print("> Seda räägivad kõik, et {} aga osta {} ära!".format(vastus,
+                                                                        asi))
     except (EOFError, KeyboardInterrupt) as e:
         print("\033[4D> Mis sul viga on, et " + asi + " ei taha ??")
         exit()
